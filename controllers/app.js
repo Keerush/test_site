e13cdc588f441dcca33dd5a3d99e1d30b74bbd8e
Brandon R <Brandon.Ramoudith@mail.utoronto.ca>
2016-09-17 19:36:07 -0400

    fixed #
    fixed some location stuff

--- controllers/app.js
+++ controllers/app.js
@@ -3,10 +3,10 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: './views/homepage.html'
+		templateUrl: 'views/homepage.html'
 	})
 	.when('/about_us', {
-		templateUrl: './views/about_us.html'
+		templateUrl: 'views/about_us.html'
 	})
 	.otherwise({ 
 		redirectTo: '/' 
